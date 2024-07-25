05e756e9747e3a670f36b6a5f77cb4298f5f5ef5
rybat117 <rybat117@gmail.com>
2024-07-25 10:42:14 -0700

    implemented player sprite

--- source/src/graphics.cpp
+++ source/src/graphics.cpp
@@ -1,15 +1,37 @@
-#include "../header/graphics.h"
 #include <SDL.h>
+#include <SDL_image.h>
 
-/*  Graphics class
- *  Holds all graphics information for the game
- */
+#include "../header/graphics.h"
+#include "../header/globals.h"
 
 Graphics::Graphics() {
-	SDL_CreateWindowAndRenderer(640, 480, 0, &this->_window, &this->_renderer); // Create window; specify resolution
+	SDL_CreateWindowAndRenderer(globals::SCREEN_WIDTH, globals::SCREEN_HEIGHT, 0, &this->_window, &this->_renderer); // Create window; specify resolution
 	SDL_SetWindowTitle(this->_window, "New Game");
 }
 
 Graphics::~Graphics() {
 	SDL_DestroyWindow(this->_window);
+}
+
+SDL_Surface* Graphics::loadImage(const std::string &filePath) {
+	if (this->_spriteSheets.count(filePath) == 0) {
+		this->_spriteSheets[filePath] = IMG_Load(filePath.c_str());
+	}
+	return this->_spriteSheets[filePath];
+}
+
+void Graphics::blitSurface(SDL_Texture* texture, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle) {
+	SDL_RenderCopy(this->_renderer, texture, sourceRectangle, destinationRectangle);
+}
+
+void Graphics::flip() {
+	SDL_RenderPresent(this->_renderer);
+}
+
+void Graphics::clear() {
+	SDL_RenderClear(this->_renderer);
+}
+
+SDL_Renderer* Graphics::getRenderer() const {
+	return this->_renderer;
 }
