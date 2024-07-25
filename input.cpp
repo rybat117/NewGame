9089ba79be84783e1e4a4258f4c3f1c37ef923d1
rybat117 <rybat117@gmail.com>
2024-07-24 18:09:18 -0700

    added controls

---
+++ input.cpp
@@ -0,0 +1,38 @@
+#include "input.h"
+
+/* Input class
+* Tracks keyboard inputs
+*/
+
+// This function is called each frame to reset irrelevant keys
+void Input::beginNewFrame() {
+	this->_pressedKeys.clear();
+	this->_releasedKeys.clear();
+}
+
+// Called when a key is pressed
+void Input::keyDownEvent(const SDL_Event& event) {
+	this->_pressedKeys[event.key.keysym.scancode] = true;
+	this->_heldKeys[event.key.keysym.scancode] = true;
+}
+
+// Called when a key is released
+void Input::keyUpEvent(const SDL_Event& event) {
+	this->_releasedKeys[event.key.keysym.scancode] = true;
+	this->_heldKeys[event.key.keysym.scancode] = false;
+}
+
+// Check if given key was pressed in current frame
+bool Input::wasKeyPressed(SDL_Scancode key) {
+	return this->_pressedKeys[key];
+}
+
+// Check if given key was released in current frame
+bool Input::wasKeyReleased(SDL_Scancode key) {
+	return this->_releasedKeys[key];
+}
+
+// Check if a given key is currently being held
+bool Input::isKeyHeld(SDL_Scancode key) {
+	return this->_heldKeys[key];
+}
--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 * The entry point of the program
 */
 
-#include "graphics.h"
+#include "game.h"
 
 int main(int argc, char** argv) {
-	Graphics graphics;
-	while (true) {
-
-	}
+	Game game;
 
 	return 0;
 }
